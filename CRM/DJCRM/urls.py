14d153090a647c1b6e55eb596e50c31a8acc7bf3
sa666u <alex_ivkov@yahoo.com>
2021-04-11 19:30:36 +0300

    Static files and Auth

--- CRM/DJCRM/urls.py
+++ CRM/DJCRM/urls.py
@@ -1,13 +1,17 @@
-from CRM.DJCRM.settings import STATIC_URL
+from os import name
 from django.conf import settings
 from django.conf.urls.static import static
 from leads.views import LandingPageView
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls', namespace="leads")),
-    path('', LandingPageView.as_view(), name="landing-page")
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', LandingPageView.as_view(), name="landing-page"),
+    path('login/', LoginView.as_view(), name="login")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
