4ca788eaa243058a6f27e3ae6ee83f696f884e24
alivkov <alex_ivkov@yagoo.com>
2023-12-17 17:02:46 +0200

    App transition

--- CRM/leads/models.py
+++ CRM/leads/models.py
@@ -4,17 +4,28 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class Lead(models.Model):
-    first_name=models.CharField(max_length=20)
-    last_name=models.CharField(max_length=20)
-    age = models.IntegerField(default=0)
-    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
+
+class Machine(models.Model):
+    machine_id = models.CharField(max_length=6, unique=True)
+    name = models.CharField(max_length=50)
+    date_of_first_inspection = models.DateField()
+    date_of_last_inspection = models.DateField()
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Inspection(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    date = models.DateField()
+    inspector = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    comments = models.TextField()
+
+    def __str__(self):
+        return self.machine.name
